f4f69ecf58bf0d060821478a333ad5e255f0963c
Sam <sampharris2000@hotmail.co.uk>
2023-10-03 16:19:37 +0100

    Spelling Error

--- components/HomeSections/Features.tsx
+++ components/HomeSections/Features.tsx
@@ -40,7 +40,7 @@ export default function Features() {
             <div className="mx-auto max-w-2xl lg:text-center">
             <h2 className="text-base font-semibold leading-7 text-red-800">Fixing fundemental flaws</h2>
             <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Simplifying the spike sorting process one step and a time
+                Simplifying the spike sorting process one step at a time
             </p>
             <p className="mt-6 text-lg leading-8 text-gray-600">
                 Spike sorting is a slow and complex procedure. Or at least, it was. <br /> These tools have the potential to fix this. Here{"'"}s how...
